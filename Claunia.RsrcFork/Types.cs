32ba3e1b55ce406a4ea85799ca04249780805403
Natalia Portillo <claunia@claunia.com>
2021-11-09 19:46:03 +0000

    Update copyright year.

--- Claunia.RsrcFork/Types.cs
+++ Claunia.RsrcFork/Types.cs
@@ -4,7 +4,7 @@
 // Author:
 //       Natalia Portillo <claunia@claunia.com>
 //
-// Copyright (c) 2016-2019 Natalia Portillo
+// Copyright (c) 2016-2021 Natalia Portillo
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Claunia.RsrcFork
 ================
 
-Copyright © 2016-2019 Natalia Portillo <claunia@claunia.com>
+Copyright © 2016-2021 Natalia Portillo <claunia@claunia.com>
 
 Library and command line application for handling resource forks.
